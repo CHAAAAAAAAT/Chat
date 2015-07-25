325be093a22a15635692b1d30687181c8db37a32
Tqtsuki <ty.cloudywolf0210@gmail.com>
2015-07-25 23:51:31 +0900

    refs#5 run,open,receive,send,close機能の追加
    run,open,receive,send,close機能を追加しました。

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -11,22 +11,47 @@ class ChatClientHandler extends Thread{
     }
 
     public void run(){
+        
     }
     
 
     public void open() throws IOException{
- 
+        in = new BufferedReader(
+            new InputStreamReader(socket.getInputStream())
+        );
+        out = new BufferedWriter(
+            new OutputStreamWriter(socket.getOutputStream())
+        );
     }    
  
     public String receive() throws IOException{
-
+        String line = in.readLine();
+        System.out.println(line);
+        return line;
     }
     
     public void send(String message) throws IOException{
-
+        out.write(message);
+        out.write("\r\n");
+        out.flush();
     }
     
     public void close(){
+        if(in != null){
+            try{
+                in.close();
+            } catch(IOException e){ }
+        }
+        if(out != null){
+            try{
+                out.close();
+            } catch(IOException e){ }
+        }
+        if(socket != null){
+            try{
+                socket.close();
+            } catch(IOException e){ }
+        }
     }
 
 }
