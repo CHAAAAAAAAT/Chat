cb468d8ee360ef6698d258ac38fffc9993b58299
g1345521 <g1345521@cc.kyoto-su.ac.jp>
2015-07-26 00:12:28 +0900

    refs #7 help機能の追加
    runが抜けていたので追加しました。あとはhelp機能を追加しました。

--- ChatClientHandler.java
+++ ChatClientHandler.java
@@ -1,17 +1,65 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+
 
 class ChatClientHandler extends Thread{
     
     private Socket socket;
-    private BufferedReader in = null;
-    private BufferedWriter out = null;
+    private BufferedReader in;
+    private BufferedWriter out;
+    private List clients;
+    private String name;
     
-    public EchoClientHandler(Socket sock){
+
+    public ChatClientHandler(Socket sock,List clients){
+	this.socket = sock;
+	this.clients = clients;
+	this.name = "undefined" + (clients.size()+1);
+    }
+    public String getClientName(){
+	return name;
     }
 
+    
+  
     public void run(){
-        
+	try{
+	    open();//ソケットを開く		
+	    System.out.println((String)getClientName() + " connected");
+	    while(true){
+		out.write(">  ");
+		out.flush();
+		String message = receive();
+		String[] commands = message.split(" ");
+		if(commands[0].equalsIgnoreCase("post")){
+		    post(commands[1]);
+		}
+		else if(commands[0].equalsIgnoreCase("bye")){
+		    bye();
+		    break;
+		}
+		else if(commands[0].equalsIgnoreCase("help")){
+		    help();
+		}
+		else if(commands[0].equalsIgnoreCase("whoami")){
+		    whoami()
+		}
+		else if(commands[0].equalsIgnoreCase("name")){
+		    newname(commands[1]);
+		}
+		else if(commands[0].equalsIgnoreCase("users")){
+		    users();
+		}
+		else if(commands[0].equalsIgnoreCase("tell")){
+		    tell(commands[1],commands[2]);
+		}	
+	    }
+	}catch(IOException e){
+	    e.printStackTrace();
+	}finally{
+	    close();
+	}
     }
     
 
@@ -52,6 +100,11 @@ class ChatClientHandler extends Thread{
                 socket.close();
             } catch(IOException e){ }
         }
+    }
+    
+    public void help() throws IOException{
+	this.send(": help  name whoami  bye  post users tell");
+	System.out.println(": help  name whoami  bye  post users tell" );
     }
 
 }
