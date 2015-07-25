6627b0b9997f5ccb96dbc9b1910b4438fca222c2
ktskny <kenya@kny-MacBook-Air.local>
2015-07-25 23:32:57 +0900

    refs #3 CHatClientHandler.javaの作成
    ひな形を作成しました。

---
+++ ChatClientHandler.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.net.*;
+
+class ChatClientHandler extends Thread{
+    
+    private Socket socket;
+    private BufferedReader in = null;
+    private BufferedWriter out = null;
+    
+    public EchoClientHandler(Socket sock){
+    }
+
+    public void run(){
+    }
+    
+
+    public void open() throws IOException{
+ 
+    }    
+ 
+    public String receive() throws IOException{
+
+    }
+    
+    public void send(String message) throws IOException{
+
+    }
+    
+    public void close(){
+    }
+
+}
